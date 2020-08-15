ff445ba0c9c5280ada06861037ceea5b6a45a16e
suling <1443229439@qq.com>
2020-08-15 11:35:11 +0800

    添加属性

---
+++ GitModule/src/com/sl/Student.java
@@ -0,0 +1,11 @@
+package com.sl;
+
+/**
+ * @Auther: suling
+ * @Date: 2020/8/15 - 08 - 15 - 11:14
+ * @Description: com.sl
+ */
+public class Student {
+    private String name;
+    private int age ;
+}
